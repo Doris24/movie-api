a0e79210b22d91e08505eb796f16b6667962ba92
Doris24 <33037450+Doris24@users.noreply.github.com>
2021-10-14 02:06:29 +0200

    Changed add new user request

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express();
 let topMovies = [
   {
     title: 'Harry Potter and the Philosopher\'s Stone',
-    year: '2001'
+    year: '2001',
+    genre: 'fantasy',
+    director: 'Chris Columbus'
   },
   {
     title: 'Dirty Dancing',
@@ -56,7 +58,8 @@ app.use(express.static('public'));
 
 app.use(bodyParser.json());
 
-// GET requests
+// REQUESTS
+
 app.get('/', (req,res) => {
   res.send('Welcome to the movie app!');
 });
@@ -68,52 +71,45 @@ app.get('/movies', (req, res) => {
 
 // get data about movie by title
 app.get('/movies/:title', (req, res) => {
-  res.json(movies.find((movie) => {
+  let movie = topMovies.find((movie) => {
     return movie.title === req.params.title;
-  }));
+  });
+  res.json(movie);
 });
 
 // get data about genre
-app.get('/movies/:genre', (req, res) => {
+app.get('/genres/:genre', (req, res) => {
   res.send('Successful GET request returning data on a genre');
 });
 
 // get data about director
-app.get('/movies/:director', (req, res) => {
+app.get('/directors/:director', (req, res) => {
   res.send('Successful GET request returning data on a director');
 });
 
 // new user
-app.post('/users', (req, res) => {
-  let newUser = req.body;
-  if (!newUser.username) {
-    const message = 'Missing "username" in request body';
-    res.status(400).send(message);
-  } else {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).send(newUser);
-  }
+app.post('/register', (req, res) => {
+  res.send('Successful POST to add information about a new user');
 });
 
 // update username
-app.put('/users/:username', (req, res) => {
-  res.send('updated name');
+app.put('/users/:id/:username', (req, res) => {
+  res.send('Successful PUT to update username');
 });
 
 // user can add movie to favList
-app.put('/users/favList/:title', (req, res) => {
-  res.send('movie was added to favList');
+app.post('/users/:id/favList/:title', (req, res) => {
+  res.send('Successful POST to add a movie to favList');
 });
 
 // user can remove movie from favList
-app.put('/users/favList/:title', (req, res) => {
-  res.send('movie was removed from favList');
-})
+app.delete('/users/:id/favList/:title', (req, res) => {
+  res.send('Successful DELETE to remove movie from favList');
+});
 
 // user can delete user
-app.delete('/users/:id', (req, res) => {
-  res.send('user was deleted')
+app.delete('/users/:id/unregister', (req, res) => {
+  res.send('Successful DELETE to unregister user');
 });
 
 
